546be4b6474eec38da43e1b898671da6b48b46fe
Elise Mery <elisemery28@gmail.com>
2022-12-12 11:03:45 +0100

    schemas and connection mongoDB

---
+++ models/groups.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const localisationSchema = mongoose.Schema({
+    label: String,
+    latitude: Number,
+    longitude: Number,
+});
+
+const groupsSchema = mongoose.Schema({
+    admin: { type: mongoose.Schema.Types.ObjectId, ref: 'users' },
+    photo: String,
+    name: String,
+    sport: { type: mongoose.Schema.Types.ObjectId, ref: 'sports' },
+    maxMembers: Number,
+    genders: [String],
+    levels: [String],
+    ageMin: Number,
+    ageMax: Number,
+    description: String,
+    localisation: localisationSchema,
+
+});
+
+const Group = mongoose.model('groups', groupsSchema);
+
+module.exports = Group;
