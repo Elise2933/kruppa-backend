92ca6bf438daa867f2c61b7c0dfd93aaa54bb773
Sami <s.david.correa@gmail.com>
2022-12-12 14:43:58 +0100

    updated bcrypt dependency for compatibility with macOS 10.12.6

--- routes/groups.js
+++ routes/groups.js
@@ -1,4 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/search/:sport/:latitude/:longitude', (req, res) => {
+    
+    let { sport, latitude, longitude } = req.params
+
+    if(!sport && (!latitude || !longitude)) {
+        res.json({ result: true, message:'Missing or empty fields.' })
+    }
+});
+
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const User = require('../models/users');
 const { checkBody } = require('../modules/checkBody');
 const uid2 = require('uid2');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 
 router.post('/signup', (req, res) => {
