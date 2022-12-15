05f63eaf1e1b304f8ee1d74e9c01c9746c89267e
Elise Mery <elisemery28@gmail.com>
2022-12-15 12:26:15 +0100

    route upload group picture

--- routes/groups.js
+++ routes/groups.js
@@ -3,13 +3,16 @@ var router = express.Router();
 const Group = require('../models/groups');
 const Sport = require('../models/sports');
 const User = require('../models/users');
+const cloudinary = require('cloudinary').v2;
+const uniqid = require('uniqid');
+const fs = require('fs');
 
 router.post('/create', (req, res) => {
-    
+
     let { admin_id, photo, name, sport_id, maxMembers, genders, levels, ageMin, ageMax, description, label, latitude, longitude } = req.body;
 
-    if(!admin_id && !photo && !name && !sport_id && !maxMembers && !genders && !levels && !ageMin && !ageMax && !description && !label && !latitude && !longitude) {
-        res.json({ result: false, message:'Missing or empty fields.' });
+    if (!admin_id && !photo && !name && !sport_id && !maxMembers && !genders && !levels && !ageMin && !ageMax && !description && !label && !latitude && !longitude) {
+        res.json({ result: false, message: 'Missing or empty fields.' });
         return
     };
 
@@ -19,7 +22,7 @@ router.post('/create', (req, res) => {
         name,
         sport: sport_id,
         maxMembers,
-        genders, 
+        genders,
         levels,
         ageMin,
         ageMax,
@@ -33,46 +36,65 @@ router.post('/create', (req, res) => {
         }
     });
     newGroup.save().then(newEntry => {
-        res.json({result: true, message: 'New group created successfully.', data: newEntry});
+        res.json({ result: true, message: 'New group created successfully.', data: newEntry });
     });
 });
 
 router.get('/search', (req, res) => {
-    
+
     let { sport, latitude, longitude } = req.query;
 
-    if((!sport && !latitude && !longitude) || ((latitude || longitude) && (!latitude || !longitude))) {
-        res.json({ result: false, message:'Missing or empty fields.' });
+    if ((!sport && !latitude && !longitude) || ((latitude || longitude) && (!latitude || !longitude))) {
+        res.json({ result: false, message: 'Missing or empty fields.' });
         return;
     }
-    
-    Sport.findOne({label: sport})
-    .then(sportData => {
-        Group.find({
-            ...(sport && { sport: sportData._id }), 
-            ...((latitude && longitude) && {"workout_location.location" : {
-                $geoWithin: { $centerSphere: [ [ Number(longitude), Number(latitude) ] ,
-                    10 / 6378.1 ] //create a file with all consts
-            }}})
-        })
-        .populate('sport')
-        .then(groups => {
-            if(groups.length > 0) {
-                res.json({result: true, groups}) //add map function to transform the object in order to change format and contain latitude and longitude (avoids errors from long/lat by default from mongo)
-            } else {
-                res.json({result: false, message: 'No groups found.', groups})
-            }
+
+    Sport.findOne({ label: sport })
+        .then(sportData => {
+            Group.find({
+                ...(sport && { sport: sportData._id }),
+                ...((latitude && longitude) && {
+                    "workout_location.location": {
+                        $geoWithin: {
+                            $centerSphere: [[Number(longitude), Number(latitude)],
+                            10 / 6378.1] //create a file with all consts
+                        }
+                    }
+                })
+            })
+                .populate('sport')
+                .then(groups => {
+                    if (groups.length > 0) {
+                        res.json({ result: true, groups }) //add map function to transform the object in order to change format and contain latitude and longitude (avoids errors from long/lat by default from mongo)
+                    } else {
+                        res.json({ result: false, message: 'No groups found.', groups })
+                    }
+                });
         });
-    });
 });
 
 router.post('/getbytoken', (req, res) => {
-    User.findOne({ token: req.body.token})
-    .populate('group')
-    .then(data => {
-        res.json({result: true, groups: data.registrations})
-      console.log(data);
-    });
+    User.findOne({ token: req.body.token })
+        .populate('group')
+        .then(data => {
+            res.json({ result: true, groups: data.registrations })
+            console.log(data);
+        });
 })
+
+//upload group picture
+router.post('/upload', async (req, res) => {
+    const photoPath = `./tmp/${uniqid()}.jpg`;
+    const resultMove = await req.files.groupPicture.mv(photoPath);
+
+    if (!resultMove) {
+        const resultCloudinary = await cloudinary.uploader.upload(photoPath);
+        res.json({ result: true, url: resultCloudinary.secure_url });
+    } else {
+        res.json({ result: false, error: resultMove });
+    }
+    fs.unlinkSync(photoPath);
+
+});
 
 module.exports = router;
