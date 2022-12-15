ea99ce5c432296610ae6daca319ac3f28de54cd0
Sami <s.david.correa@gmail.com>
2022-12-15 10:41:41 +0100

    updated signin route to return username

--- routes/users.js
+++ routes/users.js
@@ -77,7 +77,13 @@ router.post('/signin', (req, res) => {
 
   User.findOne({ email: req.body.email }).then(data => {
     if (data && bcrypt.compareSync(req.body.password, data.hash)) {
-      res.json({ result: true, token: data.token });
+      res.json({ 
+        result: true, 
+        user: {
+          token: data.token, 
+          username: data.username
+        }
+      });
     } else {
       res.json({ result: false, error: 'User not found or wrong password' });
     }
